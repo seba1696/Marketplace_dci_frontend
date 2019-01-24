5aadb576950ed49c93dada2e5397c310834360bf
Sebastián González <seba.1696@hotmail.com>
2019-01-24 11:16:16 -0300

    cambio de distribución carpetas 2

---
+++ src/app/components/curso/curso-new/curso-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-new',
+  templateUrl: './curso-new.component.html',
+  styleUrls: ['./curso-new.component.scss']
+})
+export class CursoNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
