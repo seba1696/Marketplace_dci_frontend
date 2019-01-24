ab6cc134474aff439cef39cce5ae89efdc9a6eae
Sebastián González <seba.1696@hotmail.com>
2019-01-24 11:15:59 -0300

    cambio de distribución carpetas 2

---
+++ src/app/components/alumno/alumno-list/alumno-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumno-list',
+  templateUrl: './alumno-list.component.html',
+  styleUrls: ['./alumno-list.component.scss']
+})
+export class AlumnoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/alumno/alumno-new/alumno-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumno-new',
+  templateUrl: './alumno-new.component.html',
+  styleUrls: ['./alumno-new.component.scss']
+})
+export class AlumnoNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
