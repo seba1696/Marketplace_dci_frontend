bca4e106cb0fe3e5492dea3f68cbe2375b847c2d
Sebastián González <seba.1696@hotmail.com>
2019-01-24 11:20:03 -0300

    borrado

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,21 +4,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { ProfesorListComponent } from '../../profesor/profesor-list/profesor-list.component';
-import { ProfesorNewComponent } from '../../profesor/profesor-new/profesor-new.component';
-import { ProfesorEditComponent } from '../../profesor/profesor-edit/profesor-edit.component';
-import { CursoListComponent } from '../../curso/curso-list/curso-list.component';
-import { CursoNewComponent } from '../../curso/curso-new/curso-new.component';
-import { CursoEditComponent } from '../../curso/curso-edit/curso-edit.component';
-import { CursoDetailComponent } from '../../curso/curso-detail/curso-detail.component';
-import { ProductoListComponent } from '../../producto/producto-list/producto-list.component';
-import { ProductoNewComponent } from '../../producto/producto-new/producto-new.component';
-import { ProductoEditComponent } from '../../producto/producto-edit/producto-edit.component';
-import { AlumnoListComponent } from '../../alumno/alumno-list/alumno-list.component';
-import { AlumnoNewComponent } from '../../alumno/alumno-new/alumno-new.component';
-import { AlumnoEditComponent } from '../../alumno/alumno-edit/alumno-edit.component';
-
+import { UserProfileComponent } from '../../components/user-profile/user-profile.component';
+import { ProfesorListComponent } from '../../components/profesor/profesor-list/profesor-list.component';
+import { ProfesorNewComponent } from '../../components/profesor/profesor-new/profesor-new.component';
+import { ProfesorEditComponent } from '../../components/profesor/profesor-edit/profesor-edit.component';
+import { CursoListComponent } from '../../components/curso/curso-list/curso-list.component';
+import { CursoNewComponent } from '../../components/curso/curso-new/curso-new.component';
+import { CursoEditComponent } from '../../components/curso/curso-edit/curso-edit.component';
+import { CursoDetailComponent } from '../../components/curso/curso-detail/curso-detail.component';
+import { ProductoListComponent } from '../../components/producto/producto-list/producto-list.component';
+import { ProductoNewComponent } from '../../components/producto/producto-new/producto-new.component';
+import { ProductoEditComponent } from '../../components/producto/producto-edit/producto-edit.component';
+import { AlumnoListComponent } from '../../components/alumno/alumno-list/alumno-list.component';
+import { AlumnoNewComponent } from '../../components/alumno/alumno-new/alumno-new.component';
+import { AlumnoEditComponent } from '../../components/alumno/alumno-edit/alumno-edit.component';
+import { LoginComponent } from '../../components/login/login.component';
 
 import {
   MatButtonModule,
@@ -57,6 +57,7 @@ import {
     AlumnoListComponent,
     AlumnoNewComponent,
     AlumnoEditComponent,
+    LoginComponent
   ]
 })
 
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,20 +1,21 @@
 import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { ProfesorListComponent } from '../../profesor/profesor-list/profesor-list.component';
-import { ProfesorNewComponent } from '../../profesor/profesor-new/profesor-new.component';
-import { ProfesorEditComponent } from '../../profesor/profesor-edit/profesor-edit.component';
-import { CursoListComponent } from '../../curso/curso-list/curso-list.component';
-import { CursoEditComponent } from '../../curso/curso-edit/curso-edit.component';
-import { CursoNewComponent } from '../../curso/curso-new/curso-new.component';
-import { CursoDetailComponent } from '../../curso/curso-detail/curso-detail.component';
-import { ProductoListComponent } from '../../producto/producto-list/producto-list.component';
-import { ProductoNewComponent } from '../../producto/producto-new/producto-new.component';
-import { ProductoEditComponent } from '../../producto/producto-edit/producto-edit.component';
-import { AlumnoListComponent } from '../../alumno/alumno-list/alumno-list.component';
-import { AlumnoNewComponent } from '../../alumno/alumno-new/alumno-new.component';
-import { AlumnoEditComponent } from '../../alumno/alumno-edit/alumno-edit.component';
+import { UserProfileComponent } from '../../components/user-profile/user-profile.component';
+import { ProfesorListComponent } from '../../components/profesor/profesor-list/profesor-list.component';
+import { ProfesorNewComponent } from '../../components/profesor/profesor-new/profesor-new.component';
+import { ProfesorEditComponent } from '../../components/profesor/profesor-edit/profesor-edit.component';
+import { CursoListComponent } from '../../components/curso/curso-list/curso-list.component';
+import { CursoEditComponent } from '../../components/curso/curso-edit/curso-edit.component';
+import { CursoNewComponent } from '../../components/curso/curso-new/curso-new.component';
+import { CursoDetailComponent } from '../../components/curso/curso-detail/curso-detail.component';
+import { ProductoListComponent } from '../../components/producto/producto-list/producto-list.component';
+import { ProductoNewComponent } from '../../components/producto/producto-new/producto-new.component';
+import { ProductoEditComponent } from '../../components/producto/producto-edit/producto-edit.component';
+import { AlumnoListComponent } from '../../components/alumno/alumno-list/alumno-list.component';
+import { AlumnoNewComponent } from '../../components/alumno/alumno-new/alumno-new.component';
+import { AlumnoEditComponent } from '../../components/alumno/alumno-edit/alumno-edit.component';
+import { LoginComponent } from '../../components/login/login.component';
 
 export const AdminLayoutRoutes: Routes = [
     // {
@@ -74,4 +75,6 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'curso/curso-detail/alumno-new', component: AlumnoNewComponent },
     { path: 'curso/curso-detail/alumno-edit', component: AlumnoEditComponent },
     { path: 'alumno-list', component: AlumnoListComponent },
+    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
 ];
