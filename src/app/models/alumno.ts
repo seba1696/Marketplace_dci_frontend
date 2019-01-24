c282770a2b4e856c3b350dca530a14c1e118008b
Sebastián González <seba.1696@hotmail.com>
2019-01-24 17:45:37 -0300

    modelo de alumno agregado

---
+++ src/app/models/alumno.ts
@@ -0,0 +1,5 @@
+export class Alumno {
+    constructor(public id: number, public nombre: string, public matricula: string,
+        public credito: number, public codigoCarrera: string, public email: string,
+        public idUsuario: number) { }
+}
